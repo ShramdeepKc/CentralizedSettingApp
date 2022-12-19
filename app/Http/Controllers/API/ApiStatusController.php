0d4cea7f6d77ec8afc58cee6cbe7f9ad73d81a2d
Shramdeep Kc <shramdeepkc21@gmail.com>
2022-12-19 15:47:16 +0545

    apidevelop

--- app/Http/Controllers/API/ApiStatusController.php
+++ app/Http/Controllers/API/ApiStatusController.php
@@ -11,15 +11,32 @@ class ApiStatusController extends Controller
 {
     public function status(){
 
-        $status = DB::table('status')->get();
+        $status = DB::table('status')
+        ->join('clients', 'status.id', '=', 'status.client_id')
+            ->join('products', 'status.id', '=', 'status.product_id')
+            ->select('status.*', 'clients.name as client'  , 'products.name as product')
+            ->get();
         return response()->json([
-            'status'=>true,
-            'message'=>'status retrived',
-            'data'=>$status,
+            'status'=> true,
+            'message'=>'status listed',
+            'data'=>$status
         ]);
  
         
    
     
+    }
+    public function statusbyid($id){
+        $status = Status::find($id)
+        ->join('clients', 'status.id', '=', 'status.client_id')
+        ->join('products', 'status.id', '=', 'status.product_id')
+        ->select('status.*', 'clients.name as client'  , 'products.name as product')
+        ->get();
+    return response()->json([
+        'status'=> true,
+        'message'=>'status listed',
+        'data'=>$status
+    ]);
+
     }
 }
